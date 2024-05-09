c4def79e0c87e792e67eef396bbc8619d84d7303
Lee <94841127+galaxian@users.noreply.github.com>
2024-05-09 18:12:52 +0900

    feat: 테스트용 컨트롤러 작성

---
+++ src/main/java/com/example/runnertest/TestController.java
@@ -0,0 +1,13 @@
+package com.example.runnertest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping()
+	public String hello() {
+		return "hello";
+	}
+}
