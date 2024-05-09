1afb8b3d2d638396c998902cd4e44eebb3a4b8fb
Lee <94841127+galaxian@users.noreply.github.com>
2024-05-09 18:11:11 +0900

    initial commit

---
+++ src/main/java/com/example/runnertest/RunnertestApplication.java
@@ -0,0 +1,13 @@
+package com.example.runnertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/runnertest/RunnertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runnertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
